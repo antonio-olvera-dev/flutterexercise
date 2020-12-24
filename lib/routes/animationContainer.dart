c632e3270f2f8bc316a781a0269d8fce45b82f80
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-12-24 16:15:10 +0100

    *Animated Container*

--- lib/routes/animationContainer.dart
+++ lib/routes/animationContainer.dart
@@ -8,7 +8,73 @@ class AnimatedContainerExample extends StatelessWidget {
         title: Text('Animated Container'),
       ),
       body: Body(),
-      
+      floatingActionButton: AbrirDetalles(),
+    );
+  }
+}
+
+class AbrirDetalles extends StatefulWidget {
+  const AbrirDetalles({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _AbrirDetallesState createState() => _AbrirDetallesState();
+}
+
+class _AbrirDetallesState extends State<AbrirDetalles> {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return Foto();
+        }));
+      },
+      child: Icon(Icons.add),
+    );
+  }
+}
+
+class Foto extends StatefulWidget {
+  const Foto({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _FotoState createState() => _FotoState();
+}
+
+class _FotoState extends State<Foto> {
+  var alineacion = Alignment.bottomCenter;
+  @override
+  void initState() {
+    super.initState();
+    ini();
+  }
+
+  ini() async {
+    await Future.delayed(Duration(milliseconds: 300));
+    setState(() {
+      alineacion = Alignment.topCenter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Hero(
+        tag: 'foto',
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            child: AnimatedContainer(
+                alignment: alineacion,
+                duration: Duration(seconds: 2),
+                child: Image.asset('assets/img1.jpg')),
+          ),
+        ),
+      ),
     );
   }
 }
